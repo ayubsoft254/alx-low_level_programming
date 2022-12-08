edcc557a47709c918e6bb7a4f1438b9c70f4d8ba
ayubsoft254 <henryoayub15@gmail.com>
2022-12-07 20:08:39 -0800

    getting started with betty style

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 1 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+       	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        int length = strlen(text);
+
+        write(2, text, length);
+       	return (1);
+
+}
