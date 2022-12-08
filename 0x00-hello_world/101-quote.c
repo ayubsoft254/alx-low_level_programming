a95cc0462efcc574f0880f486d2e7e2166fd0aa3
ayubsoft254 <henryoayub15@gmail.com>
2022-12-07 23:21:20 -0800

    Korpar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char *text = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int length = strlen(text);
 
 	write(2, text, length);
