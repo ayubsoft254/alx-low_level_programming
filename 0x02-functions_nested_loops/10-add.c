fba826cfdf7bc6870cf2c74d9de40281c0f57731
ayubsoft254 <henryoayub15@gmail.com>
2022-12-13 16:44:14 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Returns the addition of two intergers
+ * @a: The first interger
+ * @b: The second interger
+ *
+ * Return: The addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry block
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int count = 2;
+	long int i = 1, j = 2;
+	long int k;
+
+	printf("%lu, ", i);
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu, ", j);
+		}
+
+		k = j;
+		j += i;
+		i = k;
+		count++;
+	}
+
+	return (0);
+}
