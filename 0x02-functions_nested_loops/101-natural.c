709aa90a816defe2cb74dff7305dbd8fb0b2bfcd
ayubsoft254 <henryoayub15@gmail.com>
2022-12-13 17:06:54 -0800

    error_001

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int i - 1;
+	int i = 1;
 	int total = 0;
 
 	while (i < 1024)
 	{
 		if (i % 3 == 0)
-			total += i;
-		else if (i % 5 == 0)
 			total += i;
 
 		i++;
