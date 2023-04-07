80bad1d0500d63bec87bb8ee339d5ba28bb6b0f7
ayubsoft254 <henryoayub15@gmail.com>
2023-04-07 14:20:21 +0300

    task 2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,8 @@
  * Return: pointer to the table
  */
 
-hash_table_t *hash_table_create(unsigned long int size) {
+hash_table_t *hash_table_create(unsigned long int size)
+ {
 	hash_table_t *res = malloc(sizeof(*res));
 	hash_node_t **arr;
 	unsigned long int i = 0;
